6e533acdd6c82dd4455702ede4a942ba8f14b01d
kanies89 <krzysztof.kaniewski@gmail.com>
2024-11-29 11:22:57 +0100

    DEPLOY

--- djangobackend/backend/settings.py
+++ djangobackend/backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-z6(s*@zbgk8%esa-h(i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] or ['*'] for development
 ALLOWED_HOST = ['psychoterapiasystemowa.herokuapp.com', '127.0.0.1']
 
 # Application definition
