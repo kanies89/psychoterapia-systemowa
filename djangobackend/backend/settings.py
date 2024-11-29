bbc1743eca6f12ecfc89e1433c35a093db37abd3
kanies89 <krzysztof.kaniewski@gmail.com>
2024-11-29 10:05:40 +0100

    DEPLOY

--- djangobackend/backend/settings.py
+++ djangobackend/backend/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
