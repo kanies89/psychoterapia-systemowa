5898b718b43fe3c6a9600ca8e5d506048c6f9c63
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-05 19:00:01 +0100

    DEPLOY

--- psychsys-frontend/next.config.ts
+++ psychsys-frontend/next.config.ts
@@ -13,13 +13,5 @@ const nextConfig: NextConfig = {
         domains: ['localhost', 'psychoterapia-systemowa.pl', 'psychsys-frontend-fde1b2b544e7.herokuapp.com/'], // Add other domains if necessary
     },
 };
-// next.config.js
-module.exports = {
-    env: {
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-        NEXT_PUBLIC_RECAPTCHA_PUBLIC_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_PUBLIC_KEY,
-    },
-};
-
 
 export default nextConfig;
