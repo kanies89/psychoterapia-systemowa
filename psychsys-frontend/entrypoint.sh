2a1a891e27e049695594435492d561617cebe12a
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-06 11:01:32 +0100

    DEPLOY

--- psychsys-frontend/entrypoint.sh
+++ psychsys-frontend/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-# Replace env variable placeholders with real values
-printenv | grep NEXT_PUBLIC_ | while IFS= read -r line; do
-  key=$(echo "$line" | cut -d "=" -f1)
-  value=$(echo "$line" | cut -d "=" -f2-)
+# Replace environment variable placeholders with real values
+printenv | grep NEXT_PUBLIC_ | while read -r line ; do
+  key=$(echo $line | cut -d "=" -f1)
+  value=$(echo $line | cut -d "=" -f2)
 
-  # Replace placeholders in .next folder
-  find /usr/src/app/.next/ -type f -exec sed -i "s|${key}|${value}|g" {} \;
+  # Replace placeholders in .next static files
+  find /app/.next/ -type f -exec sed -i "s|$key|$value|g" {} \;
 done
 
-# Execute the container's main process (CMD in Dockerfile)
+# Execute the container's main process
 exec "$@"
