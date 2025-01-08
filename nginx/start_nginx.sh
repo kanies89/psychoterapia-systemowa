b4e5dcd8e698be2be076460634e024ac6f2edd78
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-08 13:19:38 +0100

    DEPLOY

---
+++ nginx/start_nginx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Replace placeholders in nginx.conf with Heroku environment variables
+sed -i "s|{{FRONTEND_URL}}|$FRONTEND_URL|g" /etc/nginx/nginx.conf
+sed -i "s|{{BACKEND_URL}}|$BACKEND_URL|g" /etc/nginx/nginx.conf
+
+# Start Nginx
+nginx -g "daemon off;"
