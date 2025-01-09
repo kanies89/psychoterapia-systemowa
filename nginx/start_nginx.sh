5f3cc03e6acc1fd144b4867e47ed0f72acff3c13
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-09 10:19:37 +0100

    DEPLOY

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -4,8 +4,5 @@
 sed -i "s|{{FRONTEND_URL}}|$FRONTEND_URL|g" /etc/nginx/nginx.conf
 sed -i "s|{{BACKEND_URL}}|$BACKEND_URL|g" /etc/nginx/nginx.conf
 
-# Substitute the $PORT variable into the nginx.conf
-envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
 # Start Nginx
 nginx -g "daemon off;"
