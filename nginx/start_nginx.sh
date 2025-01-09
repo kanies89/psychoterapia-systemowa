22784ffc528eb5a893714aada84cb94427d7f796
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-09 08:41:07 +0100

    DEPLOY

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -4,5 +4,7 @@
 sed -i "s|{{FRONTEND_URL}}|$FRONTEND_URL|g" /etc/nginx/nginx.conf
 sed -i "s|{{BACKEND_URL}}|$BACKEND_URL|g" /etc/nginx/nginx.conf
 
+export PORT=${PORT:-5000} # Default to 5000 if PORT is not set
+
 # Start Nginx
 nginx -g "daemon off;"
