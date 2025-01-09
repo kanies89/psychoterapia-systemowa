986a12f6d3b35593d965effcfe9b3cd1db780a7a
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-09 10:41:33 +0100

    DEPLOY

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-# Replace placeholders in nginx.conf with Heroku environment variables
-sed -i "s|{{FRONTEND_URL}}|$FRONTEND_URL|g" /etc/nginx/nginx.conf
-sed -i "s|{{BACKEND_URL}}|$BACKEND_URL|g" /etc/nginx/nginx.conf
+# Ensure required environment variables are set
+if [ -z "$PORT" ] || [ -z "$FRONTEND_URL" ] || [ -z "$BACKEND_URL" ]; then
+  echo "Error: One or more required environment variables (PORT, FRONTEND_URL, BACKEND_URL) are not set."
+  exit 1
+fi
+
+# Substitute environment variables in the Nginx configuration template
+envsubst '${PORT} ${FRONTEND_URL} ${BACKEND_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx
-nginx -g "daemon off;"
+nginx -g 'daemon off;'
