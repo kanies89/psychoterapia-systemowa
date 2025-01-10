4e046885a5a942540427bb134f8e3ceecf9afcda
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-10 09:50:21 +0100

    DEPLOY

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Starting entrypoint script"
-echo "HEROKU_APP_CLIENT_URL: $HEROKU_APP_CLIENT_URL"
-echo "HEROKU_APP_BACKEND_URL: $HEROKU_APP_BACKEND_URL"
+echo "HEROKU_APP_CLIENT_URL: $FRONTEND_URL"
+echo "HEROKU_APP_BACKEND_URL: $BACKEND_URL"
 echo "PORT: $PORT"
 
 # Replace variables and start Nginx
-envsubst '${PORT} ${HEROKU_APP_CLIENT_URL} ${HEROKU_APP_BACKEND_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${PORT} ${FRONTEND_URL} ${BACKEND_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 exec nginx -g "daemon off;"
