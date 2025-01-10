2a5c6a62774879a948e223bf15170c9a3fd2335b
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-10 19:45:25 +0100

    DEPLOY

--- psychsys-backend/appointment_reminder/urls.py
+++ psychsys-backend/appointment_reminder/urls.py
@@ -18,13 +18,13 @@ from appointment.views_admin import (
 
 urlpatterns = [
 
-    path('instagram_api/instagram-embed/', instagram_embed_api, name='instagram_embed_api'),
+    path('/api/instagram_api/instagram-embed/', instagram_embed_api, name='instagram_embed_api'),
     # Include the URLs of the 'appointment' library
-    path('appointment_api/get_available_services/', get_available_services, name='get_available_services'),
+    path('/api/appointment_api/get_available_services/', get_available_services, name='get_available_services'),
     # need to be incuded the date and staff_member id in request, like:
     # /appointment/get_available_slots/?selected_date=2024-12-11&staff_member=2
-    path('appointment_api/get_available_slots/', get_available_slots, name='get_available_slots'),
-    path('appointment_api/get_staff_members/<int:service_id>/', get_staff_members, name='get_staff_members_by_service')
+    path('/api/appointment_api/get_available_slots/', get_available_slots, name='get_available_slots'),
+    path('/api/appointment_api/get_staff_members/<int:service_id>/', get_staff_members, name='get_staff_members_by_service')
 ]
 
 urlpatterns += [
