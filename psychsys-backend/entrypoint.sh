cf2fa45b6d2c7af0359defcf678affd896c2ee64
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-13 16:56:55 +0100

    DEPLOY

--- psychsys-backend/entrypoint.sh
+++ psychsys-backend/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python manage.py migrate
-find /app -name create_superuser_if_not_exists.py
-python manage.py create_superuser_if_not_exists
+python manage.py create_superuser_if_not_exist
 exec "$@"
