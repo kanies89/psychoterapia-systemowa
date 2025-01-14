abe735f370a7cc04ff5ad80a11bea165c31a053f
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-14 10:39:35 +0100

    DEPLOY

--- psychsys-backend/backend/urls.py
+++ psychsys-backend/backend/urls.py
@@ -5,7 +5,7 @@ from appointment_reminder.views import get_instagram_images, create_assessment
 urlpatterns = [
     path('', include("appointment_reminder.urls")),
     # path('appointments/<str:response_type>/', include('appointment.urls')),
-    path('api/', include('appointment.urls')),
+    path('', include('appointment.urls')),
     path('api/admin/', admin.site.urls),
     path('api/get_instagram_images/', get_instagram_images, name='get_instagram_images'),
     path('api/create_assessment/', create_assessment, name='create_assessment'),
