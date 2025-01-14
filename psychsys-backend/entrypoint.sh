3bf7ec84ac2870f472cd01e9f35ca8abac1be0ea
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-14 11:45:13 +0100

    DEPLOY

--- psychsys-backend/entrypoint.sh
+++ psychsys-backend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python manage.py makemigrations appointment
+python manage.py makemigrations appointment_reminder
 python manage.py migrate
 python manage.py create_superuser_if_not_exist
 python manage.py collectstatic --no-input
