c133d35af32436ac0fa8cc5ebac89d9a5de533b7
kanies89 <krzysztof.kaniewski@gmail.com>
2025-01-17 09:46:10 +0100

    DEPLOY

--- psychsys-backend/appointment_reminder/urls.py
+++ psychsys-backend/appointment_reminder/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import instagram_embed_api, get_available_services, get_available_slots, get_staff_members
+from .views import instagram_embed_api, get_available_services, get_available_slots, get_staff_members, server_time
 
 urlpatterns = [
 
@@ -9,6 +9,6 @@ urlpatterns = [
     # need to be incuded the date and staff_member id in request, like:
     # /appointment/get_available_slots/?selected_date=2024-12-11&staff_member=2
     path('api/appointment_api/get_available_slots/', get_available_slots, name='get_available_slots'),
-    path('api/appointment_api/get_staff_members/<int:service_id>/', get_staff_members, name='get_staff_members_by_service')
-    path('api/server-time/', views.server_time, name='server_time'),
+    path('api/appointment_api/get_staff_members/<int:service_id>/', get_staff_members, name='get_staff_members_by_service'),
+    path('api/server-time/', server_time, name='server_time')
 ]
