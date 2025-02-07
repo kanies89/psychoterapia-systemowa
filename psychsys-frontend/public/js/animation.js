05e046be9452f7e2408dad93eb68d23cce20cd45
kanies89 <krzysztof.kaniewski@gmail.com>
2025-02-07 14:45:25 +0100

    DEPLOY

--- psychsys-frontend/public/js/animation.js
+++
@@ -1,6 +0,0 @@
-/* Import locomotive-scroll */
-import "locomotive-scroll/bundled/locomotive-scroll.css";
-import LocomotiveScroll from "locomotive-scroll";
-
-/* Locomotive scroll instance */
-const locomotiveScroll = new LocomotiveScroll();
